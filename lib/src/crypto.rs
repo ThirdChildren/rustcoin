bac6043ab977224fdcfed9f3be4525e82d6ba686
Stefano Leto <sleto1999@icloud.com>
2025-07-24 17:30:07 +0200

    cpu miner

--- lib/src/crypto.rs
+++ lib/src/crypto.rs
@@ -1,10 +1,13 @@
 use crate::sha256::Hash;
+use crate::util::Saveable;
 use ecdsa::{
     Signature as ECDSASignature, SigningKey, VerifyingKey, signature::Signer, signature::Verifier,
 };
 use k256::Secp256k1;
 use k256::elliptic_curve::rand_core::OsRng;
 use serde::{Deserialize, Serialize};
+use spki::EncodePublicKey;
+use std::io::{Error as IoError, ErrorKind as IoErrorKind, Read, Result as IoResult, Write};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Signature(pub ECDSASignature<Secp256k1>);
@@ -59,5 +62,40 @@ mod signkey_serde {
     {
         let bytes = Vec::<u8>::deserialize(deserializer)?;
         Ok(super::SigningKey::from_slice(&bytes).unwrap())
+    }
+}
+
+impl Saveable for PrivateKey {
+    fn load<I: Read>(reader: I) -> IoResult<Self> {
+        ciborium::de::from_reader(reader)
+            .map_err(|_| IoError::new(IoErrorKind::InvalidData, "Failed to deserialize PrivateKey"))
+    }
+    fn save<O: Write>(&self, writer: O) -> IoResult<()> {
+        ciborium::ser::into_writer(self, writer).map_err(|_| {
+            IoError::new(IoErrorKind::InvalidData, "Failed to serialize PrivateKey")
+        })?;
+        Ok(())
+    }
+}
+// save and load as PEM
+impl Saveable for PublicKey {
+    fn load<I: Read>(mut reader: I) -> IoResult<Self> {
+        // read PEM-encoded public key into string
+        let mut buf = String::new();
+        reader.read_to_string(&mut buf)?;
+
+        // decode the public key from PEM
+        let public_key = buf
+            .parse()
+            .map_err(|_| IoError::new(IoErrorKind::InvalidData, "Failed to parse PublicKey"))?;
+        Ok(PublicKey(public_key))
+    }
+    fn save<O: Write>(&self, mut writer: O) -> IoResult<()> {
+        let s = self
+            .0
+            .to_public_key_pem(Default::default())
+            .map_err(|_| IoError::new(IoErrorKind::InvalidData, "Failed to serialize PublicKey"))?;
+        writer.write_all(s.as_bytes())?;
+        Ok(())
     }
 }
