0a5e0eea6449b0403d40dd445a6f1df1a3578dd6
Stefano Leto <sleto1999@icloud.com>
2025-07-24 12:09:57 +0200

    utility binaries

--- lib/src/util.rs
+++ lib/src/util.rs
@@ -1,9 +1,28 @@
 use crate::sha256::Hash;
 use crate::types::Transaction;
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::{Read, Result as IoResult, Write};
+use std::path::Path;
 
 #[derive(Clone, Copy, Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct MerkleRoot(Hash);
+
+pub trait Saveable
+where
+    Self: Sized,
+{
+    fn load<I: Read>(reader: I) -> IoResult<Self>;
+    fn save<O: Write>(&self, writer: O) -> IoResult<()>;
+    fn save_to_file<P: AsRef<Path>>(&self, path: P) -> IoResult<()> {
+        let file = File::create(&path)?;
+        self.save(file)
+    }
+    fn load_from_file<P: AsRef<Path>>(path: P) -> IoResult<Self> {
+        let file = File::open(&path)?;
+        Self::load(file)
+    }
+}
 
 impl MerkleRoot {
     //calculate the merkle root of a block's transactions
